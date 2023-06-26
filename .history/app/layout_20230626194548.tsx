6b4f35d77457a74d077c9d31f813b8c5d8074e21
zecht12 <gilangprimae13@gmail.com>
2023-06-26 19:46:19 +0700

    push

---
+++ .history/app/layout_20230626194548.tsx
@@ -0,0 +1,46 @@
+import { Figtree } from 'next/font/google'
+
+import getSongsByUserId from '@/actions/getSongsByUserId'
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
+import ToasterProvider from '@/providers/ToasterProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+
+import './globals.css'
+import Sidebar from '@/components/Sidebar'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'ZechtCrowd',
+  description: 'ZechtCrowd',
+}
+
+export const revalidate = 0;
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const products = await getActiveProductsWithPrices();
+  const userSongs = await getSongsByUserId();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+      <link rel="icon" href="logo-GR.png" sizes="any"/>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>
+              {children}
+            </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  )
+}
